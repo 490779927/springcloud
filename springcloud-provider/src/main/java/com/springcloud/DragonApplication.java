f89ed0b272a4150deaac3a97bea42a8593bcd4a0
xiaolongren <67666123@qq.com>
2018-12-20 14:28:40 +0800

    pom整合

--- springcloud-provider/src/main/java/com/springcloud/DragonApplication.java
+++ springcloud-provider/src/main/java/com/springcloud/DragonApplication.java
@@ -2,8 +2,12 @@ package com.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@EnableEurekaClient
+@RestController
 public class DragonApplication {
 
 	public static void main(String[] args) {
